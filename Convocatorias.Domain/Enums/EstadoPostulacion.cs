fc3e0f00b60c305b5ef07343fe3b86862dc69e3e
Rebeca <chake_rebeca@hotmail.com>
2026-04-27 00:09:26 -0300

    Add Enums

---
+++ Convocatorias.Domain/Enums/EstadoPostulacion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convocatorias.Domain.Enums
+{
+    public enum EstadoPostulacion
+    {
+        Pendiente = 1,
+        Revision = 2,
+        Aprobada = 3,
+        Rechazada = 4
+    }
+}
