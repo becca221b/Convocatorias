7b6e86c68dab4624eb804db64b9556dd338753f9
Rebeca <chake_rebeca@hotmail.com>
2026-05-03 20:49:10 -0300

    Add PosgradoStatus as a enum

---
+++ Convocatorias.Domain/Enums/PosgradoStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convocatorias.Domain.Enums
+{
+    public enum PosgradoStatus
+    {
+        EnCurso,
+        Completado,
+        NoAplica
+    }
+}
