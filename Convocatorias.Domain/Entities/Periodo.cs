ad6e0c9c2b9d7f9abbfb2ff0ccc0c9725b16efeb
Rebeca <chake_rebeca@hotmail.com>
2026-05-04 00:58:05 -0300

    delete not useful code

--- Convocatorias.Domain/Entities/Periodo.cs
+++ Convocatorias.Domain/Entities/Periodo.cs
@@ -1,27 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Convocatorias.Domain.Entities
 {
     public sealed class Periodo
     {
         public Guid Id { get; private set; }
-        public string Orden { get; private set; }
+        public int Orden { get; private set; }
         public int Cuatrimestre { get; private set; }
         public int Anio { get; private set; }
         public DateTime FechaInicio { get; private set; }
         public DateTime FechaFin { get; private set; }
         private Periodo() { }
-        public Periodo(string orden, int cuatrimestre, int anio, DateTime fechaInicio, DateTime fechaFin)
+        public Periodo(int orden, int cuatrimestre, int anio, DateTime fechaInicio, DateTime fechaFin)
         {
-            if (string.IsNullOrWhiteSpace(orden)) {
-                throw new ArgumentException("El orden del período no puede estar vacío");
+            if (orden <= 0) {
+                throw new ArgumentException("El orden del período debe ser un número positivo");
             }
             if (cuatrimestre < 1 || cuatrimestre > 2) {
                     throw new ArgumentException("El cuatrimestre debe ser 1 o 2");
+            }
+            if(anio < 2000 || anio > DateTime.UtcNow.Year + 1) {
+                throw new ArgumentException("Año inválido");
             }
             if (fechaInicio >= fechaFin) {
                 throw new ArgumentException("La fecha de inicio debe ser anterior a la fecha de fin");
