f4ab8724de3fa51693f15ab8c07609b5778c3bc1
becca221b <chake_rebeca@hotmail.com>
2026-05-18 15:43:15 -0300

    Fix response

---
+++ Convocatorias.Application/Interfaces/Repositories/IPeriodoRepository.cs
@@ -0,0 +1,10 @@
+﻿using Convocatorias.Domain.Entities;
+
+
+namespace Convocatorias.Application.Interfaces.Repositories
+{
+    public interface IPeriodoRepository
+    {
+        Task<Periodo> GetByIdAsync(Guid id);
+    }
+}
