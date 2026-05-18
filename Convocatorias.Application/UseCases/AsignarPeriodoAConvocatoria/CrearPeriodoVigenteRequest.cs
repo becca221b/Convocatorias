7693cd1397eceec065a06159d60c36daa5a60a02
becca221b <chake_rebeca@hotmail.com>
2026-05-18 15:44:18 -0300

    Add Request and Response

---
+++ Convocatorias.Application/UseCases/AsignarPeriodoAConvocatoria/CrearPeriodoVigenteRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convocatorias.Application.UseCases.AsignarPeriodoAConvocatoria
+{
+    public class CrearPeriodoVigenteRequest
+    {
+        public Guid ConvocatoriaId { get; set; }
+        public Guid PeriodoId { get; set; }
+    }
+}
---
+++ Convocatorias.Application/UseCases/AsignarPeriodoAConvocatoria/CrearPeriodoVigenteResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convocatorias.Application.UseCases.AsignarPeriodoAConvocatoria
+{
+    public class CrearPeriodoVigenteResponse
+    {
+        public Guid ConvocatoriaId { get; set; }
+        public Guid PeriodoId { get; set; }
+        public DateTime FechaInicio { get; set; }
+        public DateTime FechaFin { get; set; }
+
+        public String PeriodoNombre { get; set; }
+    }
+}
