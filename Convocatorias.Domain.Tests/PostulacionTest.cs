ad5f628778fb4a6c936b0f5e39575ca762247470
becca221b <chake_rebeca@hotmail.com>
2026-05-22 18:55:12 -0300

    Add PostulacionTest

--- Convocatorias.Domain.Tests/PostulacionTest.cs
+++ Convocatorias.Domain.Tests/PostulacionTest.cs
@@ -17,12 +17,20 @@ namespace Convocatorias.Domain.Tests
         public void Deberia_cambiar_el_estado_de_la_Postulacion()
         {
             var postulacion = new Postulacion(Guid.NewGuid(), Guid.NewGuid());
-            postulacion.CambiarEstado(EstadoPostulacion.Rechazada);
-            Assert.Equal(EstadoPostulacion.Rechazada, postulacion.Estado);
+            postulacion.CambiarEstado(EstadoPostulacion.Revision);
+            Assert.Equal(EstadoPostulacion.Revision, postulacion.Estado);
         }
 
-        
+        [Fact]
+        public void Deberia_cambiar_el_estado_de_la_Postulacion_a_Aprobada()
+        {
+            var postulacion = new Postulacion(Guid.NewGuid(), Guid.NewGuid());
+            postulacion.CambiarEstado(EstadoPostulacion.Revision);
+            postulacion.CambiarEstado(EstadoPostulacion.Aprobada);
+            Assert.Equal(EstadoPostulacion.Aprobada, postulacion.Estado);
+        }
 
+       
         //Test para caso de uso: no crear postulacion para una convocatoria que no está disponible
     }
 }
