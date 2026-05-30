dcf01278e87acacbb37f805c1a98166668523ce2
becca221b <chake_rebeca@hotmail.com>
2026-05-30 11:14:49 -0300

    add get all convocatorias use case

---
+++ Convocatorias.Application/UseCases/Convocatorias/GetAll/ConvocatoriaResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convocatorias.Application.UseCases.Convocatorias.GetAll
+{
+    public sealed record ConvocatoriaResponse
+    (
+        Guid Id,
+        string Asignatura,
+        int SedeId,
+        int FacultadId,
+        int CarreraId,
+        string Modalidad,
+        string Status
+    );
+}
