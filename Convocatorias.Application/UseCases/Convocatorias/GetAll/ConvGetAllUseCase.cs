b694e48f815032e532ded8a6d14653235a92e635
becca221b <chake_rebeca@hotmail.com>
2026-05-30 11:14:29 -0300

    Add Get All Convocatorias usecase

---
+++ Convocatorias.Application/UseCases/Convocatorias/GetAll/ConvGetAllUseCase.cs
@@ -0,0 +1,31 @@
+﻿using Convocatorias.Application.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convocatorias.Application.UseCases.Convocatorias.GetAll
+{
+    public sealed class ConvGetAllUseCase
+    {
+        private readonly IConvocatoriaRepository _repository;
+        public ConvGetAllUseCase(IConvocatoriaRepository repository)
+        {
+            _repository = repository;
+        }
+        public async Task<IReadOnlyCollection<ConvocatoriaResponse>> ExecuteAsync(CancellationToken ct = default)
+        {
+            var convocatorias = await _repository.GetAllAsync(ct);
+            return convocatorias.Select(c => new ConvocatoriaResponse(
+                c.Id,
+                c.Asignatura,
+                c.SedeId,
+                c.FacultadId,
+                c.CarreraId,
+                c.Modalidad.ToString(),
+                c.Status.ToString()
+            )).ToList();
+        }
+    }
+}
