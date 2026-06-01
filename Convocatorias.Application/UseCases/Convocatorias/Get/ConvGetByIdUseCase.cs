1f24c29c028f3dd0247fb6299195389d5818241e
becca221b <chake_rebeca@hotmail.com>
2026-06-01 20:46:46 -0300

    Get Convocatorias

---
+++ Convocatorias.Application/UseCases/Convocatorias/Get/ConvGetByIdUseCase.cs
@@ -0,0 +1,35 @@
+﻿using Convocatorias.Application.Interfaces.Repositories;
+using Convocatorias.Application.UseCases.Convocatorias.GetAll;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convocatorias.Application.UseCases.Convocatorias.Get
+{
+    public sealed class ConvGetByIdUseCase
+    {
+        private readonly IConvocatoriaRepository _repository;
+        public ConvGetByIdUseCase(IConvocatoriaRepository repository)
+        {
+            _repository = repository;
+        }
+        public async Task<ConvocatoriaResponse> ExecuteAsync(Guid id, CancellationToken ct = default)
+            {
+                // Aquí iría la lógica para obtener la convocatoria por su ID desde el repositorio
+                var convocatoria = await _repository.GetByIdAsync(id, ct);
+
+            return new ConvocatoriaResponse
+            (
+                convocatoria.Id,
+                convocatoria.Asignatura,
+                convocatoria.SedeId,
+                convocatoria.FacultadId,
+                convocatoria.CarreraId,
+                convocatoria.Modalidad.ToString(),
+                convocatoria.Status.ToString()
+            );
+                   
+        }
+}
---
+++ Convocatorias.Application/UseCases/Convocatorias/Get/GetAbiertasUseCase.cs
@@ -0,0 +1,32 @@
+﻿using Convocatorias.Application.Interfaces.Repositories;
+using Convocatorias.Application.UseCases.Convocatorias.Get;
+using Convocatorias.Application.UseCases.Convocatorias.GetAll;
+
+
+namespace Convocatorias.Application.UseCases.Convocatorias.Get
+{
+    public sealed class GetAbiertasUseCase
+    {
+        private readonly IConvocatoriaRepository _repository;
+        public GetAbiertasUseCase(IConvocatoriaRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<IReadOnlyCollection<ConvocatoriaResponse>> ExecuteAsync(CancellationToken ct = default)
+        {
+            var convocatorias = await _repository.GetAbiertasAsync(ct);
+            return convocatorias.Select(c => new ConvocatoriaResponse(
+                c.Id,
+                c.Asignatura,
+                c.SedeId,
+                c.FacultadId,
+                c.CarreraId,
+                c.Modalidad.ToString(),
+                c.Status.ToString()
+            )).ToList();
+        }
+
+
+    }
+}
