4bbbb56adcf645dcd810bb329850dff784e76148
becca221b <chake_rebeca@hotmail.com>
2026-06-03 20:47:44 -0300

    add get postulacion by convocatoria Id method

--- Convocatorias.Application/Interfaces/Repositories/IPostulacionRepository.cs
+++ Convocatorias.Application/Interfaces/Repositories/IPostulacionRepository.cs
@@ -6,11 +6,14 @@ namespace Convocatorias.Application.Interfaces.Repositories
     public interface IPostulacionRepository
     {
         Task<Postulacion?> GetByIdAsync(Guid postulacionId, CancellationToken ct = default);
+        Task<IReadOnlyCollection<Postulacion>> GetAllAsync(CancellationToken ct = default);
+        Task<IReadOnlyCollection<Postulacion>> GetPostulacionesByConvocatoriaIdAsync(Guid convocatoriaId, CancellationToken ct = default);
         Task<bool> PostulacionExistsAsync(Guid convocatoriaId, Guid candidatoId, CancellationToken ct = default);
         Task AddAsync(Postulacion postulacion, CancellationToken ct = default);
 
         Task UpdateAsync(Postulacion postulacion, CancellationToken ct = default);
         Task DeleteAsync(Guid postulacionId, CancellationToken ct = default);
+        
 
     }
 }
