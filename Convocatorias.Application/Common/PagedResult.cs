457419dba7ee52500c3b71c725265eed4c8bad44
becca221b <chake_rebeca@hotmail.com>
2026-06-12 19:50:59 -0300

    Add a PagedResult for pagination

---
+++ Convocatorias.Application/Common/PagedResult.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convocatorias.Application.Common
+{
+    public sealed class PagedResult<T>
+    {
+        public IReadOnlyCollection<T> Items { get; init; }
+        public int Page { get; init; }
+        public int PageSize { get; init; }
+        public int TotalItems { get; init; }
+        public int TotalPages => (int)Math.Ceiling((double)TotalItems / PageSize);
+        public PagedResult(int page, int pageSize, int totalItems, IReadOnlyCollection<T> items)
+        {
+            Page = page;
+            PageSize = pageSize;
+            TotalItems = totalItems;
+            Items = items;
+            
+        }
+    }
+}
