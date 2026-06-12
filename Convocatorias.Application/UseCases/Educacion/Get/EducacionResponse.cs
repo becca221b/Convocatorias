eadc0bfad07d9b070608ff2a6f70741e7f79767b
becca221b <chake_rebeca@hotmail.com>
2026-06-12 19:52:52 -0300

    Education DTO

---
+++ Convocatorias.Application/UseCases/Educacion/Get/EducacionResponse.cs
@@ -0,0 +1,21 @@
+﻿using Convocatorias.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convocatorias.Application.UseCases.Educacion.Get
+{
+    public sealed record EducacionResponse
+    (
+        
+        string Titulo,
+        int AnioGraduacion,
+        PosgradoStatus PosgradoStatus,
+        string PosgradoNombre,
+        string TipoFormacion
+
+    );
+}
