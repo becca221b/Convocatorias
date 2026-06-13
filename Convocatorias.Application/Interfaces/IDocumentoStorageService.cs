7f14d71c10c129801440c856881e3b221cc80c88
becca221b <chake_rebeca@hotmail.com>
2026-06-13 11:07:09 -0300

    Add storage

---
+++ Convocatorias.Application/Interfaces/IDocumentoStorageService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convocatorias.Application.Interfaces
+{
+    public interface IDocumentoStorageService
+    {
+        Task<string> UploadAsync(Stream fileStream, string fileName, string contentType, CancellationToken ct = default);
+        Task DeleteAsync(string fileUrl, CancellationToken ct = default);
+    }
+}
---
+++ Convocatorias.Infraestructure/Storage/S3DocumentoStorageService.cs
@@ -0,0 +1,22 @@
+﻿using Amazon.S3;
+using Convocatorias.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convocatorias.Infraestructure.Storage
+{
+    public class S3DocumentoStorageService : IDocumentoStorageService
+    {
+        private readonly IAmazonS3 _s3Client;
+        private readonly string _bucketName;
+
+        public S3DocumentoStorageService(IAmazonS3 s3Client, string bucketName)
+        {
+            _s3Client = s3Client;
+            _bucketName = bucketName;
+        }
+    }
+}
